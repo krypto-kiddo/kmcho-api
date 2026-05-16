66a0d6877b38a9ae0471a81fe02749056016eb9f
Yashwardhan <20190802138@dypiu.ac.in>
2026-05-16 12:59:53 +0530

    increase negative balance limit

--- app/routers/orders.py
+++ app/routers/orders.py
@@ -22,7 +22,7 @@ async def create_order(
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
 
-    MINIMUM_BALANCE = -5000
+    MINIMUM_BALANCE = -10000
 
     if user.current_balance - payload.amount < MINIMUM_BALANCE:
         raise HTTPException(status_code=400, detail="Insufficient balance")
